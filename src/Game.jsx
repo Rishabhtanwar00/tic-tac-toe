a6d0dabd8c99f06e055e57a135c5bbf2e3309414
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2024-12-19 00:27:31 +0530

    added wins counter and local storage support to keep track wins

--- src/Game.jsx
+++ src/Game.jsx
@@ -4,9 +4,8 @@ import { useState } from 'react';
 const Game = () => {
 	const [squares, setSquares] = useState(Array(9).fill(null));
 	const [isXNext, setIsXNext] = useState(true);
-	useEffect(() => {
-		console.log(squares);
-	}, [squares]);
+	const [wins, setWins] = useState({ X: 0, O: 0 });
+	const [winner, setWinner] = useState('');
 
 	const handleClick = (index) => {
 		if (squares[index] || getResult[squares]) return;
@@ -42,11 +41,45 @@ const Game = () => {
 		return null;
 	};
 
-	const winner = getResult(squares);
 	const getBgClass = (value) => {
 		if (value === 'X') return 'bg-green-500';
 		if (value === '0') return 'bg-red-500';
 		return 'bg-black';
+	};
+
+	useEffect(() => {
+		const newWinner = getResult(squares);
+		setWinner(newWinner);
+
+		if (newWinner) {
+			setWins((prev) => ({
+				X: newWinner === 'X' ? prev.X + 1 : prev.X,
+				O: newWinner === '0' ? prev.O + 1 : prev.O,
+			}));
+		}
+	}, [squares]);
+
+	useEffect(() => {
+		if (wins.X || wins.O) {
+			localStorage.setItem('wins', JSON.stringify(wins));
+		}
+	}, [wins]);
+
+	useEffect(() => {
+		const savedWinCount = JSON.parse(localStorage.getItem('wins'));
+		if (savedWinCount) setWins(savedWinCount);
+	}, []);
+
+	const resetGame = () => {
+		setSquares(Array(9).fill(null));
+		setIsXNext(true);
+		setWinner('');
+	};
+
+	const resetCounter = () => {
+		const resetWins = { X: 0, O: 0 };
+		setWins(resetWins);
+		localStorage.setItem('wins', JSON.stringify(resetWins));
 	};
 
 	return (
@@ -77,15 +110,28 @@ const Game = () => {
 				</p>
 			)}
 
-			<button
-				onClick={() => {
-					setSquares(Array(9).fill(null));
-					setIsXNext(true);
-				}}
-				className='bg-red-500 text-white px-5 py-2'
-			>
-				Restart
-			</button>
+			<div className='flex gap-5'>
+				<p className='px-5 py-1 bg-green-500 text-white text-base'>
+					X wins: <span className='font-bold'> {wins.X}</span>
+				</p>
+				<p className='px-5 py-1 bg-red-500 text-white text-base'>
+					0 wins: <span className='font-bold'> {wins.O}</span>
+				</p>
+			</div>
+			<div className='flex gap-5'>
+				<button
+					onClick={resetGame}
+					className='bg-orange-500 text-white px-5 py-2 active:scale-90 transition-all duration-75 ease-in-out'
+				>
+					Restart
+				</button>
+				<button
+					onClick={resetCounter}
+					className='bg-gray-500 text-white px-5 py-2 active:scale-90 transition-all duration-75 ease-in-out'
+				>
+					Reset Win Counter
+				</button>
+			</div>
 		</div>
 	);
 };
